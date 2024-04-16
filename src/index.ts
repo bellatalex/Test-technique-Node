60aee0f25fe61df9a5f3460ffc0ab917ae073fc3
bellatalex <bellata.alex@gmail.com>
2024-04-16 18:34:30 +0200

    adding authenticateToken and /api/generate_json_bridge exemple

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,24 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 const port = process.env.PORT || 3000;
+type NextFunction = (err?: any) => void;
+const master_token =
+  "8dJyzP1.:3Mbpr11y~8ZUdEY*E.m&LV?dCqT96U'H:-(I?!YGgyyuGgp2VawTJ!t'*Uw$ekuO`YeWoip~WPC/TFL&|qQXf$/W7'QVIPp0U9tnGPhuD'@GuEdZO/G^^Ect+q!enpn";
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send('Hello World');
+});
+
+function authenticateToken(req: Request, res: Response, next: NextFunction) {
+  const token_header = req.headers['token'];
+  if (token_header == master_token) {
+    return next();
+  }
+  return res.status(401).json({ error: 'Unauthorized' });
+}
+
+app.get('/api/generate_json_bridge', authenticateToken, (req, res) => {
+  res.status(200).json({ message: "Accès autorisé à l'API sécurisée" });
 });
 
 app.listen(port, () => {
